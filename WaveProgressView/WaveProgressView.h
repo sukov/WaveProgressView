5e46c2510e5a69431c481ce03bcba02d34212b80
sukov <gorjan5@hotmail.com>
2017-12-18 02:14:37 +0100

    Initial Commit

---
+++ WaveProgressView/WaveProgressView.h
@@ -0,0 +1,19 @@
+//
+//  WaveProgressView.h
+//  WaveProgressView
+//
+//  Created by Admin on 12/18/17.
+//  Copyright © 2017 Gorjan Sukov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WaveProgressView.
+FOUNDATION_EXPORT double WaveProgressViewVersionNumber;
+
+//! Project version string for WaveProgressView.
+FOUNDATION_EXPORT const unsigned char WaveProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WaveProgressView/PublicHeader.h>
+
+
